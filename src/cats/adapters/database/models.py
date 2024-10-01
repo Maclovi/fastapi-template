747a2522c672a820177dc920e96bd100f30665a9
Sergey <yavorskyserge@gmail.com>
2024-10-01 17:26:26 +0300

    add models

---
+++ src/cats/adapters/database/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, ForeignKey, Integer, MetaData, String, Table
+from sqlalchemy.orm import registry
+
+from cats.domain.models import Breed, Cat
+
+metadata = MetaData()
+
+cats = Table(
+    "cats",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("color", String),
+    Column("age", Integer),
+    Column("description", String),
+    Column("breed_id", ForeignKey("breeds.id"), nullable=True),
+)
+
+breeds = Table(
+    "breeds",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String),
+)
+
+mapper_registry = registry()
+mapper_registry.map_imperatively(Breed, breeds)
+mapper_registry.map_imperatively(Cat, cats)
