20c1632115743811744c93ef3f3fa428d9ce68c4
Sergey <yavorskyserge@gmail.com>
2024-10-01 19:05:16 +0300

    change path to /all

--- src/cats/api/breeds.py
+++ src/cats/api/breeds.py
@@ -5,6 +5,6 @@ from cats.domain.models import Breed
 breeds_router = APIRouter(prefix="/breeds")
 
 
-@breeds_router.get("/breeds", response_model=list[Breed])
+@breeds_router.get("/all", response_model=list[Breed])
 async def get_all_breeds() -> list[Breed]:
     return [Breed(title="muy"), Breed(title="muy2"), Breed(title="muy3")]
