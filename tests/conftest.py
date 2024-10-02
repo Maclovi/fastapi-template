d0842210cd5029b5b7b396109b9eb7c3608dd0f3
Sergey <yavorskyserge@gmail.com>
2024-10-02 19:56:02 +0300

    remove init_dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from cats.api import breeds, cats, index
-from cats.di import init_dependencies
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -12,7 +11,5 @@ def client() -> TestClient:
     app.include_router(index.index_router)
     app.include_router(cats.cats_router)
     app.include_router(breeds.breeds_router)
-
-    init_dependencies(app)
 
     return TestClient(app)
