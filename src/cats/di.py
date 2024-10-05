8ad0a7dc22067f1a7b8a520972d818d5ba42516c
Sergey <yavorskyserge@gmail.com>
2024-10-05 11:42:10 +0300

    refactoring: cute style

--- src/cats/di.py
+++ src/cats/di.py
@@ -16,18 +16,29 @@ from cats.domain.services import BreedService, CatService
 
 
 class CatServiceProvider:
-    pass
+    @classmethod
+    def cat_provider(
+        cls, session: AsyncSession = Depends(Stub(AsyncSession))
+    ) -> CatService:
+        return CatService(
+            CatRepository(session), BreedRepository(session), session
+        )
 
 
 class BreedServiceProvider:
-    pass
+    @classmethod
+    def breed_provider(
+        cls,
+        session: AsyncSession = Depends(Stub(AsyncSession)),
+    ) -> BreedService:
+        return BreedService(BreedRepository(session), session)
 
 
 def create_engine() -> AsyncEngine:
-    db_uri = os.getenv("POSTGRES_URI")
-    if not db_uri:
-        raise ValueError("POSTGRES_URI is not set")
-
+    db_uri = os.getenv(
+        "POSTGRES_URI",
+        "postgresql+psycopg://postgres:postgres@postgres:5432/defaultdb",
+    )
     return create_async_engine(
         db_uri,
         echo=True,
@@ -58,12 +69,6 @@ def cat_provider(
     )
 
 
-def breed_provider(
-    session: AsyncSession = Depends(Stub(AsyncSession)),
-) -> BreedService:
-    return BreedService(BreedRepository(session), session)
-
-
 def init_dependencies(app: FastAPI) -> None:
     engine = create_engine()
     session_maker = create_async_sessionmaker(engine)
@@ -71,5 +76,9 @@ def init_dependencies(app: FastAPI) -> None:
     app.dependency_overrides[AsyncSession] = partial(
         new_session, session_maker
     )
-    app.dependency_overrides[CatServiceProvider] = cat_provider
-    app.dependency_overrides[BreedServiceProvider] = breed_provider
+    app.dependency_overrides[CatServiceProvider] = (
+        CatServiceProvider.cat_provider
+    )
+    app.dependency_overrides[BreedServiceProvider] = (
+        BreedServiceProvider.breed_provider
+    )
