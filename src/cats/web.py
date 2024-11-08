72925df7a63deb6b9ef0f2956b49e19bd55c60e0
Sergey <yavorskyserge@gmail.com>
2024-11-08 16:26:57 +0300

    decomposite create_app

--- src/cats/web.py
+++ src/cats/web.py
@@ -13,22 +13,34 @@ logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+async def _lifespan(app: FastAPI) -> AsyncIterator[None]:
     yield None
     await app.state.dishka_container.close()
 
 
-def create_app() -> FastAPI:
+def _setup_conf_logger() -> None:
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s  %(process)-7s %(module)-20s %(message)s",
     )
-    app = FastAPI(lifespan=lifespan)
+
+
+def _setup_container(app: FastAPI, /) -> None:
+    container = init_async_container()
+    setup_dishka(container, app)
+
+
+def _setup_routers(app: FastAPI, /) -> None:
     app.include_router(cats.router)
     app.include_router(breeds.router)
     app.include_router(index.router)
-    container = init_async_container()
-    setup_dishka(container, app)
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(lifespan=_lifespan)
+    _setup_container(app)
+    _setup_routers(app)
+    _setup_conf_logger()
     map_tables()
     logger.info("App created")
     return app
